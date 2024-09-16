14d20cd20f8a4efa1e24ffae0bd2f78a1944b2fd
DaphnieCampanilla <146432973+DaphnieCampanilla@users.noreply.github.com>
2024-09-16 20:33:04 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExcercise {
+   public static void main (String[] args) {
+      System.out.println("First Java application");
+      displayAddress();
+   }
+   
+   public static void displayAddress() {
+      System.out.println ("Vivo Company");
+      System.out.println ("Vivo Building, No. 8, Wangdao Avenue");
+      System.out.println ("Dongguan, Guangdong 523808");
+      System.out.println ("Company: Vivo Communication Technology Co. Ltd."); 
+   } 
+}
+
